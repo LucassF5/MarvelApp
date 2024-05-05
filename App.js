763e31296a89b27f6eb5e2317241cf7b2be44379
L F <franco100lucas@gmail.com>
2024-05-05 16:17:21 -0300

    feat: Iniciando Apps.js e bibliotecas

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
+import { NavigationContainer } from "@react-navigation/native"
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Iniciando trabalho</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#000" barStyle={'default'} />
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
