adf9d9c182bfdabfd8364b31d60cb1378a5d68b6
L F <franco100lucas@gmail.com>
2024-05-07 22:57:57 -0300

    feat: Atualizando tela de cadastro

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,123 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, ImageBackground } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Login() {
   const navigation = useNavigation()
   return (
-    <View>
-      <Text>Login</Text>
-      <TouchableOpacity style={styles.voltar} onPress={()=>navigation.goBack()}>
-        <Text>Voltar</Text>
-      </TouchableOpacity>
-    </View>
+
+    <ImageBackground source={require("../../assets/M.jpg")} resizeMode='cover' style={styles.image}>
+      <LinearGradient
+        colors={['rgba(0,0,0,0.8)', 'rgba(0,0,0,0.7)', 'rgba(0,0,0,0.7)', 'rgba(0,0,0,0.7)', 'rgba(0,0,0,0.8)']}
+      // style={styles.container}
+      >
+        <View style={styles.container}>
+          <Text style={styles.cadastro}>Cadastro de Usuário</Text>
+          <Text style={{ color: "white", textAlign: "center", marginTop: "5%" }}>Complete com suas informações</Text>
+          <View style={styles.form}>
+            <Text
+              style={styles.text}
+            >Nome Completo</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Nome"
+            />
+            {/* <Text
+              style={styles.text}
+            >Email</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Nome"
+            /> */}
+            <Text
+              style={styles.text}
+            >Senha</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="* * * * * * * * "
+            />
+            <Text
+              style={styles.text}
+            >Confirmar Senha</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="* * * * * * * * "
+            />
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Welcome")}>
+              <Text
+                style={styles.buttonText}
+              >Registrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </LinearGradient>
+    </ImageBackground>
   )
 }
 
 styles = StyleSheet.create({
-  voltar: {
-    backgroundColor: 'blue',
-    color: 'white'
+  container: {
+  // flex: 1,
+  // justifyContent: 'center',
+  // alignItems: 'center',
+  // backgroundColor: 'rgba(0,0,0,0.8)',
+  height: "100%"
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  cadastro: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: "18%"
+  },
+  form: {
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: "10%",
+    marginTop: "10%"
+  },
+  text: {
+    color: 'white',
+    marginBottom: 5,
+    alignSelf: 'flex-start',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: "3%"
+  },
+  textInput: {
+    backgroundColor: 'white',
+    width: "100%",
+    height: 40,
+    borderRadius: 5,
+    // color: 'white',
+    marginBottom: "5%",
+    paddingLeft: "3%",
+    height: 55,
+    fontSize: 18
+  },
+  button: {
+    backgroundColor: 'white',
+    width: "100%",
+    height: 40,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: "10%",
+    height: "11%"
+  },
+  buttonText: {
+    color: 'rgb(33, 21, 33)',
+    fontSize: 23
   }
 })
+
+{/* <Text>Email</Text>
+<Text>Senha</Text>
+<Text>Telefone</Text>
+<Text>Confirmar senha</Text> */}
